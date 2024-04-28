cb33306bf6175f6abacfaecdb198e14aa6032eaa
sharuys <118631570+sharuys@users.noreply.github.com>
2024-04-28 16:37:34 +0300

    Add files via upload

---
+++ Ciname.DAL/Models/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinema.DAL.Models
+{
+    [Table("bookings")]
+    public class Booking
+    {
+        [Key]
+        [Column("booking_id")]
+        public int BookingID { get; set; }
+
+        [Column("user_id")]
+        public int UserID { get; set; }
+
+        [Column("session_id")]
+        public int SessionID { get; set; }
+
+        [Column("quantity")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ Ciname.DAL/Models/Session.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinema.DAL.Models
+{
+    [Table("sessions")]
+    public class Session
+    {
+        [Key]
+        [Column("session_id")]
+        public int SessionID { get; set; }
+
+        [Column("film_id")]
+        public int FilmID { get; set; }
+
+        [Column("date_time")]
+        public DateTime DateTime { get; set; }
+
+        [Column("ticket_price")]
+        public decimal TicketPrice { get; set; }
+
+        [Column("hall")]
+        public string Hall { get; set; } 
+
+        [Column("language")]
+       
+        public bool Is3D { get; set; } 
+
+        [Column("is_big_hall")]
+        public bool IsBigHall { get; set; } 
+
+    }
+}
